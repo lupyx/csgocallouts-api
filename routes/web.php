cd074c93e79eb461d0e03ea99b3fc80ca63dbff3
Tanguy Deleu <prostyle592@gmail.com>
2017-06-23 02:42:33 +0200

    Added routes for following actions: Maps, Map by ID, Add Map

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->group(['prefix' => 'api'], function() use ($app) {
+
+    $app->get('/map/{id}', 'Api/MapsController@map');
+    $app->get('/maps', 'Api/MapsController@maps');
+    $app->post('/map', 'Api/MapsController@addMap');
+});
