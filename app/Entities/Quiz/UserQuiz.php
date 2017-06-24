9d43f09b134999cd93a2e38fc269f31f283211a9
Tanguy Deleu <prostyle592@gmail.com>
2017-06-24 18:07:13 +0200

    Added relationships to model

--- app/Entities/Quiz/UserQuiz.php
+++ app/Entities/Quiz/UserQuiz.php
@@ -4,8 +4,22 @@
 namespace App\Entities\Quiz;
 
 
-class UserQuiz
+use App\Entities\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class UserQuiz extends Model
 {
     protected $table = 'users_quizzes';
     protected $hidden = [ 'pivot', 'created_at', 'updated_at' ];
+
+    public function user() : HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function quiz() : HasOne
+    {
+        return $this->hasOne(Quiz::class);
+    }
 }
