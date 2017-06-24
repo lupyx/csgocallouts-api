9e0f5f4e30e30c56eec57514e34b88a510ff4414
Tanguy Deleu <prostyle592@gmail.com>
2017-06-24 18:46:33 +0200

    Updated to inherit TranslatableModel

--- app/Entities/Quiz/Question.php
+++ app/Entities/Quiz/Question.php
@@ -3,44 +3,15 @@
 
 namespace App\Entities\Quiz;
 
+use App\Entities\Base\TranslatableModel;
 use App\Entities\Callout;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class Question extends Model
+class Question extends TranslatableModel
 {
     protected $fillable = [ 'content' ];
     protected $hidden = [ 'pivot', 'created_at', 'updated_at' ];
-
-    /**
-     * @param string $locale The language identifier
-     * @return bool Does a translation for in the specified language exist?
-     * Will return whether or not there is a translation for the specified locale
-     */
-    public function hasTranslation(string $locale, Connection $connection) : bool
-    {
-        $translation = $connection->table('questions_localisations')->where([
-            'quiz_id' => $this->id,
-            'lang' => $locale
-        ])->get();
-
-        return !is_null($translation);
-    }
-
-    /**
-     * @param string $locale The language identifier
-     * Will replace the title of the quiz with the translated version
-     */
-    public function translate(string $locale, Connection $connection) : void
-    {
-        $translation = $connection->table('questions_localisations')->where([
-            'quiz_id' => $this->id,
-            'lang' => $locale
-        ])->first('title');
-
-        $this->title = $translation;
-    }
+    protected $translatable = [ 'content' ];
 
     public function answer() : HasOne
     {
--- app/Entities/Quiz/Quiz.php
+++ app/Entities/Quiz/Quiz.php
@@ -3,43 +3,15 @@
 
 namespace App\Entities\Quiz;
 
+use App\Entities\Base\TranslatableModel;
 use App\Entities\Map;
-use Illuminate\Database\Connection;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class Quiz extends Model
+class Quiz extends TranslatableModel
 {
     protected $hidden = [ 'created_at', 'updated_at' ];
-    /**
-     * @param string $locale The language identifier
-     * @return bool Does a translation for in the specified language exist?
-     * Will return whether or not there is a translation for the specified locale
-     */
-    public function hasTranslation(string $locale, Connection $connection) : bool
-    {
-        $translation = $connection->table('quizzes_localisations')->where([
-            'quiz_id' => $this->id,
-            'lang' => $locale
-        ])->get();
-
-        return !is_null($translation);
-    }
-
-    /**
-     * @param string $locale The language identifier
-     * Will replace the title of the quiz with the translated version
-     */
-    public function translate(string $locale, Connection $connection) : void
-    {
-        $translation = $connection->table('quizzes_localisations')->where([
-            'quiz_id' => $this->id,
-            'lang' => $locale
-        ])->first('title');
-
-        $this->title = $translation;
-    }
+    protected $translatable = [ 'title' ];
 
     public function questions() : BelongsToMany
     {
