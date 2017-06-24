d926deddd986ac69de30defd49074735c3665e22
Tanguy Deleu <prostyle592@gmail.com>
2017-06-24 19:18:30 +0200

    Fixed mistake

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ $app->get('/', function () use ($app) {
 
 // SteamAuth register
 $app->post('/steamuser/{steamId64}', 'SteamAuthController@registerUser');
-$app->get('/steam/auth', 'SteamAuthController@getAuthenticationUrl');
+$app->get('/steam/auth', 'SteamAuthController@getAuthentication');
 
 $app->get('/map/{id}', 'MapsController@map');
 $app->get('/maps', 'MapsController@maps');
