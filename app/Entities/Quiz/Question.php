2ca86aa0934465c85508dc13c595d39b5f3479e1
Tanguy Deleu <prostyle592@gmail.com>
2017-06-26 17:47:58 +0200

    Added prepareForAnswer method which will fill it with possible answers

--- app/Entities/Quiz/Question.php
+++ app/Entities/Quiz/Question.php
@@ -6,6 +6,7 @@ namespace App\Entities\Quiz;
 use App\Entities\Base\TranslatableModel;
 use App\Entities\Callout;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Question extends TranslatableModel
 {
@@ -13,8 +14,37 @@ class Question extends TranslatableModel
     protected $hidden = [ 'pivot', 'created_at', 'updated_at' ];
     protected $translatable = [ 'content' ];
 
+    public $preparedAnswers = [];
+
+    /**
+     * @return BelongsTo
+     */
     public function answer() : BelongsTo
     {
         return $this->belongsTo(Callout::class, 'questions_answers');
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function quizzes() : BelongsToMany
+    {
+        return $this->belongsToMany(Quiz::class, 'quizzes_questions');
+    }
+
+
+    /**
+     * @param int $amountOfChoices The amount of choices (correct answer included) this method should generate
+     * Will generate the preparedAnswers[] property of this class including the right answer and wrong answers
+     */
+    public function prepareForAnswer(int $amountOfChoices = 3) : void
+    {
+        $this->preparedAnswers[] = $this->answer;
+        $wrongAnswers = Callout::whereNotIn('id', $this->answer->id)->inRandomOrder()->take($amountOfChoices - 1);
+
+        foreach($wrongAnswers as $wrongAnswer)
+            $this->preparedAnswers[] = $wrongAnswer;
+
+        $this->preparedAnswers = shuffle($this->preparedAnswers);
     }
 }
