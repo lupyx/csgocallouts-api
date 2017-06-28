96b4c7b8cf6981058a65f1cf28a88e69a99711e0
Tanguy Deleu <prostyle592@gmail.com>
2017-06-28 04:07:25 +0200

    Corrected id mistake

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -16,7 +16,7 @@ class QuestionTableSeeder extends Seeder
             ['id' => 1, 'content' => 'What is this spot called?', 'answer_id' => 1],
             ['id' => 2, 'content' => 'Where is the terrorist standing?', 'answer_id' => 2],
             ['id' => 3, 'content' => 'What are the two spots the terrorists are standing on?', 'answer_id' => 3],
-            ['id' => 3, 'content' => 'What are the two spots the terrorists are standing on?', 'answer_id' => 4],
+            ['id' => 4, 'content' => 'What are the two spots the terrorists are standing on?', 'answer_id' => 4],
         ];
 
         Question::insert($questions);
